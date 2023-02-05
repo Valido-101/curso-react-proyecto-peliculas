0e18a313670a7a1b4fbb9f130ab2019be7108484
Valido-101 <jesuszafra22@gmail.com>
2023-02-05 20:33:08 +0100

    Componente y comprobación para editar creado

--- src/components/Listado.js
+++ src/components/Listado.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react'
+import { Editar } from './Editar';
 
 export const Listado = ({listadoState, setListadoState}) => {
 
     //const [listadoState, setListadoState] = useState([]);
+    const [editar, setEditar] = useState(0);
 
     useEffect(() => {
 
@@ -15,6 +17,22 @@ export const Listado = ({listadoState, setListadoState}) => {
         let peliculas = JSON.parse(localStorage.getItem("pelis"));
 
         setListadoState(peliculas);
+
+        return peliculas;
+    }
+
+    const borrarPeli = (id) => {
+        //Conseguir películas almacenadas
+        let pelis_almacenadas = conseguirPeliculas();
+
+        //Filtrar esas películas para que se elimine la que quiero
+        let nuevo_array_peliculas = pelis_almacenadas.filter(peli => peli.id !== parseInt(id));
+
+        //Actualizar estado del listado
+        setListadoState(nuevo_array_peliculas);
+
+        //Actualizar los datos en el local storage
+        localStorage.setItem('pelis', JSON.stringify(nuevo_array_peliculas));
     }
 
   return (
@@ -25,8 +43,14 @@ export const Listado = ({listadoState, setListadoState}) => {
                         <h3 className="title">{peli.titulo}</h3>
                         <p className="description">{peli.descripcion}</p>
 
-                        <button className="edit">Editar</button>
-                        <button className="delete">Borrar</button>
+                        <button className="edit" onClick={() => {setEditar(peli.id)}}>Editar</button>
+                        <button className="delete" onClick={() => borrarPeli(peli.id)}>Borrar</button>
+
+                        {/*aparece formulario edición*/}
+                        {editar === peli.id && (
+                            <Editar/>
+                        )}
+
                     </article>);})
             :
             <h2>No hay películas surmano</h2>
