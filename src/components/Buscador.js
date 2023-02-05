49b447ed1eb24090c7150b4d3023ed6b40570fe0
Valido-101 <jesuszafra22@gmail.com>
2023-02-05 21:12:35 +0100

    Buscador creado y funcional

--- src/components/Buscador.js
+++ src/components/Buscador.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export const Buscador = () => {
+export const Buscador = ({listadoState, setListadoState}) => {
+
+  const [busqueda, setBusqueda] = useState('');
+
+  const buscarPeli = (e) => {
+    //crear estado y actuaizarlo
+    setBusqueda(e.target.value)
+
+    //filtrar para buscar coincidencias
+    let pelis_encontradas = listadoState.filter(peli => {
+      return peli.titulo.toLowerCase().includes(busqueda.toLowerCase())
+    });
+
+    if (busqueda.length <= 1 || pelis_encontradas <= 0) {
+      pelis_encontradas = JSON.parse(localStorage.getItem("pelis"));
+    }
+
+    //actualizar estado del listado con lo que he filtrado
+    setListadoState(pelis_encontradas);
+  }
+
   return (
     <div className="search">
-        <h3 className="title">Buscador</h3>
+        <h3 className="title">Buscador: {busqueda}</h3>
         <form>
-            <input type="text" id="search_field" />
+            <input type="text" 
+                  id="search_field"
+                  name="busqueda"
+                  autoComplete='off'
+                  value={busqueda}
+                  onChange={buscarPeli} />
             <button id="search">Buscar</button>
         </form>
     </div>
