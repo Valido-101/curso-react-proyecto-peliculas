333a3f656b7cb8c189e111807f3de87647ddadae
Valido-101 <jesuszafra22@gmail.com>
2023-02-05 21:18:23 +0100

    Buscador con mensaje terminado

--- src/components/Buscador.js
+++ src/components/Buscador.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 export const Buscador = ({listadoState, setListadoState}) => {
 
   const [busqueda, setBusqueda] = useState('');
+  const [noEncontrado, setNoEncontrado] = useState(false);
 
   const buscarPeli = (e) => {
     //crear estado y actuaizarlo
@@ -15,6 +16,9 @@ export const Buscador = ({listadoState, setListadoState}) => {
 
     if (busqueda.length <= 1 || pelis_encontradas <= 0) {
       pelis_encontradas = JSON.parse(localStorage.getItem("pelis"));
+      setNoEncontrado(true);
+    }else{
+      setNoEncontrado(false);
     }
 
     //actualizar estado del listado con lo que he filtrado
@@ -24,6 +28,8 @@ export const Buscador = ({listadoState, setListadoState}) => {
   return (
     <div className="search">
         <h3 className="title">Buscador: {busqueda}</h3>
+        {noEncontrado == true && 
+        (<span className='no-encontrado'>No se ha encontrado ninguna coincidencia</span>)}
         <form>
             <input type="text" 
                   id="search_field"
